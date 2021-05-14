fb850c54ee759dd7d0614d808f60fa28aa907f69
Cruz Molina <cruz.adam.molina@gmail.com>
2021-05-14 11:43:43 -0700

    Build: Initial Dockerfile setup (#1)
    * build: initial package.json
    
    * build: generate initial package-lock
    
    * docs: initial README
    
    * build: initial Dockerfile
    
    * build: initial docker run command script

---
+++ scripts/runDockerCommand.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Runs a command input as an environment variable.
+# Allows docker image to run arbitrary commands by specifying them in the
+# environment rather than the `docker run` command.
+$RUN_COMMAND
