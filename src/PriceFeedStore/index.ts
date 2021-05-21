1af89baa688e1d603856fa8b7fcbf265ebaf742f
Cruz Molina <cruz.adam.molina@gmail.com>
2021-05-21 11:36:51 -0700

    Feat: initia PriceFeedStore (#8)
    * feat: add initial minimal Chainlink Aggregator ABI
    
    * feat: add & export chainlinkAggregatorProxyContract to helpers/ether & helpers/index
    
    * feat: initial PriceFeedStore for Chainlink Price Feed data

---
+++ src/PriceFeedStore/index.ts
@@ -0,0 +1,86 @@
+import { chainlinkAggregatorProxyContract, Logger } from "../helpers";
+
+export default class PriceFeedStore {
+  public latestRoundData: any;
+
+  constructor() {
+    this.latestRoundData = {};
+  }
+
+  public getLatestRoundData() {
+    return this.latestRoundData;
+  }
+
+  start = () => {
+    Logger.info({
+      at: "PriceFeedStore#start",
+      message: "Starting price feed store",
+    });
+    this._subscribe();
+  };
+
+  _fetchLatestRoundData = async () => {
+    try {
+      const { answer, roundId, updatedAt } =
+        await chainlinkAggregatorProxyContract.latestRoundData();
+
+      this.latestRoundData = { answer, roundId, updatedAt };
+
+      Logger.info({
+        at: "PriceFeedStore#_fetchLatestRoundData",
+        message: "Price feed store initialized",
+        answer: answer.toNumber(),
+        roundId: roundId.toString(),
+        updatedAt: updatedAt.toNumber(),
+      });
+    } catch (error) {
+      Logger.error({
+        at: "PriceFeedStore#_fetchLatestRoundData",
+        message: error.message,
+        error,
+      });
+    }
+  };
+
+  _subscribe = async () => {
+    await this._fetchLatestRoundData();
+
+    Logger.info({
+      at: "PriceFeedStore#_subscribe",
+      message: "Updating price feed store...",
+    });
+
+    try {
+      this._subscribeToAnswerUpdatedEvents();
+    } catch (error) {
+      Logger.error({
+        at: "PriceFeedStore#_subscribe",
+        message: error.message,
+        error,
+      });
+      this._subscribe();
+    }
+  };
+
+  _subscribeToAnswerUpdatedEvents = async () => {
+    const chainlinkAggregatorContract = chainlinkAggregatorProxyContract.attach(
+      await chainlinkAggregatorProxyContract.aggregator()
+    );
+
+    chainlinkAggregatorContract.on(
+      "AnswerUpdated",
+      (answerPrice, roundId, updatedTimestamp) => {
+        this.latestRoundData = {
+          answer: answerPrice,
+          roundId,
+          updatedAt: updatedTimestamp,
+        };
+
+        Logger.info({
+          at: "PriceFeedStore#_subscribeToAnswerUpdatedEvents",
+          message: "Price feed store updated",
+        });
+      }
+    );
+  };
+}
