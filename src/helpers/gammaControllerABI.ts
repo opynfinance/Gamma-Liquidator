599795b4d877ea7ecb15b1bd0f29ea09afa0c0fd
Cruz Molina <cruz.adam.molina@gmail.com>
2021-05-24 15:54:46 -0700

    Refactor: VaultStore (#11)
    * refactor: add getVault fn to Gamma Controller ABI
    
    * refactor: retrieve & store shortAmount, shortOtokenAddress, & vaultId in VaultStore on opened margin vault events
    
    * refactor: update package.json urls

--- src/helpers/gammaControllerABI.ts
+++ src/helpers/gammaControllerABI.ts
@@ -3,6 +3,7 @@ const gammaControllerABI = [
   "event VaultLiquidated(address indexed liquidator, address indexed receiver, address indexed vaultOwner, uint256 debt, uint256 auctionPrice, uint256 collateralPayout, uint256 auctionStartingRound)",
   "event VaultOpened(address indexed accountOwner, uint256 vaultId, uint256 indexed vaultType)",
   "event VaultSettled(address indexed AccountOwner, address indexed to, address indexed otoken, uint256 vaultId, uint256 payout)",
+  "function getVault(address _owner, uint256 _vaultId) public view",
   "function isLiquidatable(address _owner, uint256 _vaultId, uint256 _roundId) external view returns (bool, uint256, uint256)",
   "function operate((uint8 actionType, address owner, address secondAddress, address asset, uint256 vaultId, uint256 amount, uint256 index, bytes data)[] memory _actions) external",
 ];
