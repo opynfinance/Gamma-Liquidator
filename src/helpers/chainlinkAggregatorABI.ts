b50a6f60c425ed6fa7696dca424c469f22bdc2b8
Cruz Molina <cruz.adam.molina@gmail.com>
2021-06-11 17:46:52 -0700

    Feat: Extend PriceFeedStore (#17)
    * refactor: add description fn to Chainlink Aggregator ABI
    
    * feat: add _fetchPriceFeedPair method to fetch, parse, and store PriceFeedStore.underlyingAsset

--- src/helpers/chainlinkAggregatorABI.ts
+++ src/helpers/chainlinkAggregatorABI.ts
@@ -2,6 +2,7 @@
 const chainlinkAggregatorABI = [
   "event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 updatedAt)",
   "function aggregator() external view returns (address)",
+  "function description() external view returns (string)",
   "function latestRoundData() public view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)",
 ];
 
