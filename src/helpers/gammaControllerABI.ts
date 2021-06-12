e58fe6d57aaa69a4d18878b0cd736f63dcab1021
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-11 17:48:15 -0700

    refactor: add getAccountVaultCounter fn to Gamma Controller ABI

--- src/helpers/gammaControllerABI.ts
+++ src/helpers/gammaControllerABI.ts
@@ -1,6 +1,7 @@
 // minimal contract ABI needed for liquidations
 const gammaControllerABI = [
   "event VaultOpened(address indexed accountOwner, uint256 vaultId, uint256 indexed vaultType)",
+  "function getAccountVaultCounter(address _accountOwner) external view returns (uint256)",
   "function getVault(address _owner, uint256 _vaultId) public view",
   "function isLiquidatable(address _owner, uint256 _vaultId, uint256 _roundId) external view returns (bool, uint256, uint256)",
   "function operate((uint8 actionType, address owner, address secondAddress, address asset, uint256 vaultId, uint256 amount, uint256 index, bytes data)[] memory _actions) external",
