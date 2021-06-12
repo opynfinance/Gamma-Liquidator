4d899b43ef82f1d314df5dae5072624b3c010c39
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-11 18:16:15 -0700

    feat: initial Liquidator for executing liquidations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 import GasPriceStore from "./GasPriceStore";
-// import Liquidator from "./Liquidator";
+import Liquidator from "./Liquidator";
 import PriceFeedStore from "./PriceFeedStore";
 import VaultStore from "./VaultStore";
 import { loadLiquidatorAccount, Logger } from "./helpers";
@@ -15,7 +15,7 @@ async function start() {
   const gasPriceStore = new GasPriceStore();
   const priceFeedStore = new PriceFeedStore();
   const vaultStore = new VaultStore();
-  // const liquidator = new Liquidator(priceFeedStore, vaultStore);
+  const liquidator = new Liquidator(gasPriceStore, priceFeedStore, vaultStore);
 
   await loadLiquidatorAccount();
 
@@ -23,7 +23,7 @@ async function start() {
   priceFeedStore.start();
   vaultStore.start();
 
-  //     liquidator.start();
+  liquidator.start();
 }
 
 start();
