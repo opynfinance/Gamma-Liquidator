62542fb1c2543af5aeabe7730510ebefdc1a1710
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-14 16:35:44 -0700

    refactor: re-org fetchLiquidatableVaults helper logic

--- src/Liquidator/helpers/liquidations/fetchLiquidatableVaults.ts
+++ src/Liquidator/helpers/liquidations/fetchLiquidatableVaults.ts
@@ -1,5 +1,3 @@
-import { BigNumber } from "ethers";
-
 import Liquidator from "../../index";
 import { gammaControllerProxyContract, Logger } from "../../../helpers";
 
@@ -10,42 +8,29 @@ export default async function fetchLiquidatableVaults(Liquidator: Liquidator) {
   for (const vaultOwnerAddress of vaultOwnerAddresses) {
     await Promise.all(
       nakedMarginVaults[vaultOwnerAddress].map(async (vaultId) => {
-        let answer = BigNumber.from(0),
-          currentRoundIdCalculatedAuctionPrice = BigNumber.from(0),
-          isUnderCollateralized,
-          roundId = BigNumber.from(0),
-          vaultDetails;
         try {
-          [vaultDetails] = await gammaControllerProxyContract.getVault(
+          const [vaultDetails] = await gammaControllerProxyContract.getVault(
             vaultOwnerAddress,
             vaultId
           );
 
           if (!vaultDetails) return;
-        } catch (error) {
-          Logger.error({
-            at: "Liquidator#_initialLiquidationAttempt",
-            message: error.message,
-            error,
-          });
-        }
 
-        // currently only one vault per vaultId
-        // here we check if the vault is no longer short
-        if (vaultDetails.shortAmounts[0].eq(0)) return;
+          // currently only one vault per vaultId
+          // here we check if the vault is no longer short
+          if (vaultDetails.shortAmounts[0].eq(0)) return;
 
-        // currently only one collateralAsset address per vault
-        const collateralAssetAddress = vaultDetails.collateralAssets[0];
-        // currently only one short position per vault
-        const shortAmount = vaultDetails.shortAmounts[0];
-        // currently only one shortOtoken address per vault
-        const shortOtokenAddress = vaultDetails.shortOtokens[0];
+          // currently only one collateralAsset address per vault
+          const collateralAssetAddress = vaultDetails.collateralAssets[0];
+          // currently only one short position per vault
+          const shortAmount = vaultDetails.shortAmounts[0];
+          // currently only one shortOtoken address per vault
+          const shortOtokenAddress = vaultDetails.shortOtokens[0];
 
-        try {
-          ({ answer, roundId } =
-            Liquidator.priceFeedStore.getLatestRoundData());
+          const { answer, roundId } =
+            Liquidator.priceFeedStore.getLatestRoundData();
 
-          [isUnderCollateralized, currentRoundIdCalculatedAuctionPrice] =
+          const [isUnderCollateralized, currentRoundIdCalculatedAuctionPrice] =
             await gammaControllerProxyContract.isLiquidatable(
               vaultOwnerAddress,
               vaultId,
@@ -53,69 +38,70 @@ export default async function fetchLiquidatableVaults(Liquidator: Liquidator) {
             );
 
           if (!isUnderCollateralized) return;
-        } catch (error) {
-          Logger.error({
-            at: "Liquidator#_initialLiquidationAttempt",
-            message: error.message,
-            error,
-          });
-        }
 
-        if (Liquidator.liquidatableVaults[vaultOwnerAddress]) {
-          let vaultPresent = false;
-          await Promise.all(
-            Liquidator.liquidatableVaults[vaultOwnerAddress].map(
-              async (vault) => {
-                if (vaultId.eq(vault.vaultId)) {
-                  vaultPresent = true;
+          if (Liquidator.liquidatableVaults[vaultOwnerAddress]) {
+            let vaultPresent = false;
+            await Promise.all(
+              Liquidator.liquidatableVaults[vaultOwnerAddress].map(
+                async (vault) => {
+                  if (vaultId.eq(vault.vaultId)) {
+                    vaultPresent = true;
 
-                  if (!roundId.eq(vault.roundId)) {
-                    const [, oldRoundIdRecalculatedAuctionPrice] =
-                      await gammaControllerProxyContract.isLiquidatable(
-                        vaultOwnerAddress,
-                        vaultId,
-                        vault.roundId
-                      );
-                    if (
-                      currentRoundIdCalculatedAuctionPrice.gt(
-                        oldRoundIdRecalculatedAuctionPrice
-                      )
-                    ) {
-                      vault.latestAuctionPrice =
-                        currentRoundIdCalculatedAuctionPrice;
-                      vault.roundId = roundId;
-                    } else {
-                      vault.latestAuctionPrice =
-                        oldRoundIdRecalculatedAuctionPrice;
+                    if (!roundId.eq(vault.roundId)) {
+                      const [, oldRoundIdRecalculatedAuctionPrice] =
+                        await gammaControllerProxyContract.isLiquidatable(
+                          vaultOwnerAddress,
+                          vaultId,
+                          vault.roundId
+                        );
+
+                      if (
+                        currentRoundIdCalculatedAuctionPrice.gt(
+                          oldRoundIdRecalculatedAuctionPrice
+                        )
+                      ) {
+                        vault.latestAuctionPrice =
+                          currentRoundIdCalculatedAuctionPrice;
+                        vault.roundId = roundId;
+                      } else {
+                        vault.latestAuctionPrice =
+                          oldRoundIdRecalculatedAuctionPrice;
+                      }
                     }
                   }
                 }
-              }
-            )
-          );
-          if (!vaultPresent) {
-            Liquidator.liquidatableVaults[vaultOwnerAddress].push({
-              latestAuctionPrice: currentRoundIdCalculatedAuctionPrice,
-              latestUnderlyingAssetPrice: answer,
-              collateralAssetAddress,
-              roundId,
-              shortAmount,
-              shortOtokenAddress,
-              vaultId,
-            });
+              )
+            );
+            if (!vaultPresent) {
+              Liquidator.liquidatableVaults[vaultOwnerAddress].push({
+                latestAuctionPrice: currentRoundIdCalculatedAuctionPrice,
+                latestUnderlyingAssetPrice: answer,
+                collateralAssetAddress,
+                roundId,
+                shortAmount,
+                shortOtokenAddress,
+                vaultId,
+              });
+            }
+          } else {
+            Liquidator.liquidatableVaults[vaultOwnerAddress] = [
+              {
+                latestAuctionPrice: currentRoundIdCalculatedAuctionPrice,
+                latestUnderlyingAssetPrice: answer,
+                collateralAssetAddress,
+                roundId,
+                shortAmount,
+                shortOtokenAddress,
+                vaultId,
+              },
+            ];
           }
-        } else {
-          Liquidator.liquidatableVaults[vaultOwnerAddress] = [
-            {
-              latestAuctionPrice: currentRoundIdCalculatedAuctionPrice,
-              latestUnderlyingAssetPrice: answer,
-              collateralAssetAddress,
-              roundId,
-              shortAmount,
-              shortOtokenAddress,
-              vaultId,
-            },
-          ];
+        } catch (error) {
+          Logger.error({
+            at: "Liquidator#_fetchLiquidatableVaults",
+            message: error.message,
+            error,
+          });
         }
       })
     );
