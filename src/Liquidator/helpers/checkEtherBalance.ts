165d2e3a35dfc20f27a3badb9bf723f534a8089f
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-16 17:54:05 -0700

    feat: add Liquidator checkEtherBalance helper fn to check BOT_MINIMUM_ETHER_BALANCE

---
+++ src/Liquidator/helpers/checkEtherBalance.ts
@@ -0,0 +1,20 @@
+import { BigNumber } from "ethers";
+
+import { liquidatorAccount, Logger } from "../../helpers";
+
+export default async function checkEtherBalance() {
+  const liquidatorAccountBalance = await liquidatorAccount.getBalance();
+  if (
+    liquidatorAccountBalance.lt(
+      BigNumber.from(process.env.BOT_MINIMUM_ETHER_BALANCE)
+    )
+  ) {
+    Logger.error({
+      at: "Liquidator#checkEtherBalance",
+      message: "Liquidator account balance less than BOT_MINIMUM_ETHER_BALANCE",
+      error: Error(
+        "Liquidator account balance less than BOT_MINIMUM_ETHER_BALANCE"
+      ),
+    });
+  }
+}
