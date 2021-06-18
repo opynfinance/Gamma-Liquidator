d139eff75a937882c38758b254467753dbeb29ca
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-18 10:00:27 -0700

    feat: add fetchNakedMarginVaults helper, subgraph query for naked margin vaults

---
+++ src/VaultStore/fetchNakedMarginVaults.ts
@@ -0,0 +1,72 @@
+import { BigNumber } from "ethers";
+import fetch from "node-fetch";
+
+import supportedNetworkSubgraphs from "./supportedNetworkSubgraphs";
+import { INakedMarginVaults } from "./types";
+import { liquidatorAccountAddress, Logger, provider } from "../helpers";
+
+export default async function fetchNakedMarginVaults(): Promise<INakedMarginVaults> {
+  const networkChainId = (await provider.getNetwork()).chainId.toString();
+
+  const subgraphUrl = supportedNetworkSubgraphs[networkChainId];
+
+  if (!subgraphUrl) {
+    Logger.error({
+      at: "VaultStore#fetchNakedMarginVaults",
+      message: "Warning: No subgraph for detected Ethereum network",
+    });
+    return {};
+  }
+
+  const nakedMarginVaultQuery = JSON.stringify({
+    query: `
+      {
+        vaults(where: { owner_not: "${liquidatorAccountAddress}" type: 1 }) {
+          owner {
+            address: id
+            }
+          vaultId
+        }
+      }`,
+  });
+
+  try {
+    const {
+      data: { vaults },
+    } = await (
+      await fetch(subgraphUrl, {
+        body: nakedMarginVaultQuery,
+        method: "POST",
+      })
+    ).json();
+
+    const nakedMarginVaults: INakedMarginVaults = vaults.reduce(
+      (
+        nakedMarginVaults: INakedMarginVaults,
+        {
+          owner: { address },
+          vaultId,
+        }: { owner: { address: string }; vaultId: string }
+      ) => {
+        if (!nakedMarginVaults[address]) nakedMarginVaults[address] = [];
+        return {
+          ...nakedMarginVaults,
+          [address]: [
+            ...nakedMarginVaults[`${address}`],
+            BigNumber.from(vaultId),
+          ],
+        };
+      },
+      {}
+    );
+
+    return nakedMarginVaults;
+  } catch (error) {
+    Logger.error({
+      at: "VaultStore#fetchNakedMarginVaults",
+      message: error.message,
+      error,
+    });
+    return {};
+  }
+}
