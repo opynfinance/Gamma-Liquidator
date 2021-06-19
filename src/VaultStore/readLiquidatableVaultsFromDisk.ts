ae9f3ba1d7946b052e2f352870896ceedd8152a5
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-06-18 17:08:40 -0700

    feat: add readLiquidatableVaultsFromDisk helper

---
+++ src/VaultStore/readLiquidatableVaultsFromDisk.ts
@@ -0,0 +1,21 @@
+import { ILiquidatableVaults } from "./types";
+import { Logger } from "../helpers";
+import db from "./database";
+
+export default async function readLiquidatableVaultsFromDisk(): Promise<ILiquidatableVaults> {
+  try {
+    return JSON.parse(await db.get("liquidatableVaults"));
+  } catch (error) {
+    if (error.notFound) {
+      return {};
+    }
+
+    Logger.error({
+      at: "VaultStore#readLiquidatableVaultsFromDisk",
+      message: error.message,
+      error,
+    });
+
+    return {};
+  }
+}
