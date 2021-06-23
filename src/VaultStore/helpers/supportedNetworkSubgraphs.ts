51fe3c2654c33282b31fd21ac541ba6440b4c456
Cruz Molina <cruz.adam.molina@gmail.com>
2021-06-23 09:03:08 -0700

    refactor: add internal ropsten subgraph (#39)

--- src/VaultStore/helpers/supportedNetworkSubgraphs.ts
+++ src/VaultStore/helpers/supportedNetworkSubgraphs.ts
@@ -1,7 +1,7 @@
 export const supportedNetworkSubgraphs: { [chainId: string]: string } = {
   "1": "https://api.thegraph.com/subgraphs/name/opynfinance/gamma-mainnet", // mainnet subgraph
-  "3": "https://api.thegraph.com/subgraphs/name/opynfinance/gamma-ropsten", // ropsten subgraph
-  "42": "https://api.thegraph.com/subgraphs/name/opynfinance/gamma-internal-kovan", // kovan subgraph
+  "3": "https://api.thegraph.com/subgraphs/name/opynfinance/gamma-internal-ropsten", // internal ropsten subgraph
+  "42": "https://api.thegraph.com/subgraphs/name/opynfinance/gamma-internal-kovan", // internal kovan subgraph
 };
 
 export default supportedNetworkSubgraphs;
