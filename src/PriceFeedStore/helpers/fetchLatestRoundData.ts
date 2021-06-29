be1c9d40ed6be19316060d833378d44bfc04d982
Cruz Molina <cruz.adam.molina@gmail.com>
2021-06-29 09:39:47 -0700

    Fix: Gamma Controller ABI (#46)
    * fix: add return values to Gamma Controller ABI for getVaultWithDetails
    
    * refactor: misc

--- src/PriceFeedStore/helpers/fetchLatestRoundData.ts
+++ src/PriceFeedStore/helpers/fetchLatestRoundData.ts
@@ -1,23 +1,25 @@
 import PriceFeedStore from "..";
 import { chainlinkAggregatorProxyContract, Logger } from "../../helpers";
 
-export default async function fetchPriceFeedPair(
+export default async function fetchLatestRoundData(
   PriceFeedStore: PriceFeedStore
 ): Promise<void> {
   try {
-    const priceFeedPair = await chainlinkAggregatorProxyContract.description();
+    const { answer, roundId, updatedAt } =
+      await chainlinkAggregatorProxyContract.latestRoundData();
 
-    PriceFeedStore.setUnderlyingAsset(priceFeedPair.match(/([^\s]+)/g)[0]);
+    PriceFeedStore.setLatestRoundData({ answer, roundId, updatedAt });
 
     Logger.info({
-      at: "PriceFeedStore#fetchPriceFeedPair",
-      message: "Price feed underlying asset set",
-      priceFeedPair,
-      underlyingAsset: PriceFeedStore.getUnderlyingAsset(),
+      at: "PriceFeedStore#fetchLatestRoundData",
+      message: "Price feed store initialized",
+      answer: answer.toNumber(),
+      roundId: roundId.toString(),
+      updatedAt: updatedAt.toNumber(),
     });
   } catch (error) {
     Logger.error({
-      at: "PriceFeedStore#fetchPriceFeedPair",
+      at: "PriceFeedStore#fetchLatestRoundData",
       message: error.message,
       chainlinkAggregatorProxyContractAddress:
         chainlinkAggregatorProxyContract.address,
