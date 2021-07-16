51709bd4ad4d650e2e40efab9454de586f8ac243
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-07-16 12:15:48 -0700

    refactor: add isSettlementAllowed to Gamma Controller ABI

--- src/helpers/gammaControllerABI.ts
+++ src/helpers/gammaControllerABI.ts
@@ -4,6 +4,7 @@ const gammaControllerABI = [
   "function getAccountVaultCounter(address _accountOwner) external view returns (uint256)",
   "function getVaultWithDetails(address _owner, uint256 _vaultId) public view returns ((address[] shortOtokens, address[] longOtokens, address[] collateralAssets, uint256[] shortAmounts, uint256[] longAmounts, uint256[] collateralAmounts), uint256, uint256)",
   "function isLiquidatable(address _owner, uint256 _vaultId, uint256 _roundId) external view returns (bool, uint256, uint256)",
+  "function isSettlementAllowed(address _otoken) external view returns (bool)",
   "function operate((uint8 actionType, address owner, address secondAddress, address asset, uint256 vaultId, uint256 amount, uint256 index, bytes data)[] memory _actions) external",
 ];
 
