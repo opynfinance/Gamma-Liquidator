7ed43ae7fc3c14775d9848636c9148679a288c57
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-07-16 12:16:11 -0700

    refactor: add isSettlementAllowed check when calculating settleable vaults

--- src/Liquidator/helpers/settlements/calculateSettleableVaults.ts
+++ src/Liquidator/helpers/settlements/calculateSettleableVaults.ts
@@ -1,23 +1,27 @@
 import { BigNumber } from "ethers";
 
 import Liquidator from "../../index";
+import { gammaControllerProxyContract } from "../../../helpers";
 
-export default function calculateSettleableVaults(
+export default async function calculateSettleableVaults(
   { vaultStore }: Liquidator,
   updatedTimestamp: BigNumber
-): Liquidator["vaultStore"]["settleableVaults"] {
+): Promise<Liquidator["vaultStore"]["settleableVaults"]> {
   const settlementVaults = vaultStore.getSettlementVaults();
   const settlementVaultNonces = Object.keys(settlementVaults);
   const settleableVaults = vaultStore.getSettleableVaults();
 
-  for (const settlementVaultNonce of settlementVaultNonces) {
+  for await (const settlementVaultNonce of settlementVaultNonces) {
     const settlementVault = settlementVaults[settlementVaultNonce];
     const shortOtokenAddress = Object.keys(settlementVault)[0];
     const settlementDetails = settlementVault[shortOtokenAddress];
 
     if (
       updatedTimestamp.gt(settlementDetails.expiryTimestamp) &&
-      settlementDetails.shortAmount.gt(0)
+      settlementDetails.shortAmount.gt(0) &&
+      (await gammaControllerProxyContract.isSettlementAllowed(
+        shortOtokenAddress
+      ))
     ) {
       settleableVaults[settlementVaultNonce] = settlementDetails.shortAmount;
     }
