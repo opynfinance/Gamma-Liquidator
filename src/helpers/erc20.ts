d303a23d16b7c77f10bc5027e8d1d19f522eb4ce
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-07-20 15:13:23 -0700

    refactor: re-add minimal ERC-20 ABI

---
+++ src/helpers/erc20.ts
@@ -0,0 +1,8 @@
+// minimal contract ABI needed for fetching ERC-20 info
+const erc20ABI = [
+  "function allowance(address owner, address spender) external view returns (uint256)",
+  "function balanceOf(address account) external view returns (uint256)",
+  "function decimals() public view returns (uint8)",
+];
+
+export default erc20ABI;
