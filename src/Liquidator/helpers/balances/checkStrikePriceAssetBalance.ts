5f0b1e70e784fd0fcb2791bbda9036293c02fcc4
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-07-20 15:47:01 -0700

    refactor: use collateralCustodianAddress to check strike price asset balances

--- src/Liquidator/helpers/balances/checkStrikePriceAssetBalance.ts
+++ src/Liquidator/helpers/balances/checkStrikePriceAssetBalance.ts
@@ -1,38 +1,63 @@
 import { BigNumber, utils } from "ethers";
 
 import {
+  collateralCustodianAddress,
   liquidatorAccountAddress,
   Logger,
   strikePriceAssetContract,
 } from "../../../helpers";
 
 export default async function checkStrikePriceAssetBalance(): Promise<void> {
-  const liquidatorAccountStrikePriceAssetBalance =
-    await strikePriceAssetContract.balanceOf(liquidatorAccountAddress);
+  const strikePriceAssetBalance = await strikePriceAssetContract.balanceOf(
+    collateralCustodianAddress
+  );
 
   if (
-    liquidatorAccountStrikePriceAssetBalance.lt(
+    strikePriceAssetBalance.lt(
       BigNumber.from(process.env.BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE)
     )
   ) {
     const strikePriceAssetDecimals = await strikePriceAssetContract.decimals();
 
+    if (collateralCustodianAddress !== liquidatorAccountAddress) {
+      Logger.error({
+        at: "Liquidator#checkStrikePriceAssetBalance",
+        message:
+          "Collateral custodian strike price asset balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE",
+        BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE: utils.formatUnits(
+          process.env.BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE as string,
+          strikePriceAssetDecimals
+        ),
+        collateralCustodianAddress,
+        collateralCustodianBalance: utils.formatUnits(
+          strikePriceAssetBalance,
+          strikePriceAssetDecimals
+        ),
+        strikePriceAssetAddress: process.env.STRIKE_PRICE_ASSET_ADDRESS,
+        error: Error(
+          "Collateral custodian strike price asset balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE."
+        ),
+      });
+
+      return;
+    }
+
     Logger.error({
       at: "Liquidator#checkStrikePriceAssetBalance",
       message:
-        "Liquidator account balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE",
+        "Liquidator account strike price asset balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE",
       BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE: utils.formatUnits(
         process.env.BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE as string,
         strikePriceAssetDecimals
       ),
       liquidatorAccountAddress,
       liquidatorAccountBalance: utils.formatUnits(
-        liquidatorAccountStrikePriceAssetBalance,
+        strikePriceAssetBalance,
         strikePriceAssetDecimals
       ),
       strikePriceAssetAddress: process.env.STRIKE_PRICE_ASSET_ADDRESS,
       error: Error(
-        "Liquidator account balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE."
+        "Liquidator account strike price asset balance less than BOT_MINIMUM_STRIKE_PRICE_ASSET_BALANCE."
       ),
     });
   }
