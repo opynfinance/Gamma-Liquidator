90122e35e04f25ca240ae83e8f2496e201017b8a
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2021-07-21 16:19:34 -0700

    feat: initial transferOtokens helper

---
+++ src/Liquidator/helpers/liquidations/transferOtokens.ts
@@ -0,0 +1,54 @@
+import { ethers } from "ethers";
+
+import Liquidator from "../..";
+import {
+  collateralCustodianAddress,
+  erc20ABI,
+  liquidatorAccount,
+  liquidatorAccountAddress,
+  Logger,
+} from "../../../helpers";
+
+export default async function transferOtokens(
+  Liquidator: Liquidator,
+  {
+    shortOtokenAddress,
+    shortAmount,
+  }: Liquidator["vaultStore"]["liquidatableVaults"][string][number]
+): Promise<void> {
+  const shortOtokenContract = new ethers.Contract(
+    shortOtokenAddress,
+    erc20ABI,
+    liquidatorAccount
+  );
+
+  try {
+    const transaction = await shortOtokenContract.transferFrom(
+      collateralCustodianAddress,
+      liquidatorAccountAddress,
+      shortAmount,
+      { gasPrice: Liquidator.gasPriceStore.getLastCalculatedGasPrice() }
+    );
+
+    await shortOtokenContract.provider.waitForTransaction(
+      transaction.hash,
+      undefined,
+      Number(process.env.EXPIRED_TRANSACTION_TIMEOUT) // default 60 seconds
+    );
+
+    return;
+  } catch (error) {
+    Logger.error({
+      alert: "Critical error during oToken transferFrom attempt",
+      at: "Liquidator#transferOtokens",
+      message: error.message,
+      collateralCustodianAddress,
+      liquidatorAccountAddress,
+      shortAmount: shortAmount.toString(),
+      shortOtokenAddress,
+      error,
+    });
+
+    return;
+  }
+}
